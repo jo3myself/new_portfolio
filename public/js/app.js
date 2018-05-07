53d8c50ecd002b3be52fe90a7f112a68ef3e0a34
CODING <CODING>
2018-05-07 13:23:17 -0700

    adding new skills

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,32 @@
 new WOW().init()
 
 const skills = {
-  web: ['React.js', 'HTML', 'CSS', 'JavaScript', 'jQuery', 'Responsive Design', 'Bootstrap', 'Material UI', 'Handlebars.js', 'C#', 'ASP.NET', 'Express.js', 'Node.js', 'AJAX', 'APIs', 'MVC'],
+  web: ['React.js', 'Redux', 'HTML', 'CSS', 'JavaScript', 'jQuery', 'C#', 'ASP.NET', 'Responsive Design', 'Bootstrap', 'Material UI', 'Handlebars.js', 'Express.js', 'Node.js', 'AJAX', 'APIs', 'MVC'],
   database: ['MySQL', 'MongoDB', 'SQL Server', 'Firebase', 'Ms Access'],
   deployment: ['GIT', 'Heroku'],
-  tools: ['Sublime', 'Visual Studio Code', 'Photoshop', 'Notepad']
+  tools: ['Sublime', 'Visual Studio', 'Photoshop', 'Notepad']
 }
 
-for (let w = 0; w < skills.web.length; w++) {
+skills.web.map(web => {
   let skill = $('<span>')
   skill.addClass('badge badge-pill stylish-color-dark')
-  skill.append('<i class="fa fa-code"></i> ' + skills.web[w])
+  skill.append('<i class="fa fa-code"></i> ' + web)
   $('#web').append(skill)
-}
+})
 
-for (let w = 0; w < skills.database.length; w++) {
+for (data of skills.database) {
   let skill = $('<span>')
   skill.addClass('badge badge-pill stylish-color-dark')
-  skill.append('<i class="fa fa-database"></i> ' + skills.database[w])
+  skill.append('<i class="fa fa-database"></i> ' + data)
   $('#database').append(skill)
 }
 
-for (let w = 0; w < skills.deployment.length; w++) {
+skills.deployment.forEach((deploy) => {
   let skill = $('<span>')
   skill.addClass('badge badge-pill stylish-color-dark')
-  skill.append('<i class="fa fa-code-fork"></i> ' + skills.deployment[w])
+  skill.append('<i class="fa fa-code-fork"></i> ' + deploy)
   $('#deployment').append(skill)
-}
+})
 
 for (let w = 0; w < skills.tools.length; w++) {
   let skill = $('<span>')
